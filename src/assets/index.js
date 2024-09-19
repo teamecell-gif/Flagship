f0bc0f3bc55afd0ab7fbad970059c7f5d02e168f
venpom76568 <hagrawal560@gmail.com>
2024-09-19 07:56:40 +0530

    23 to 24

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,4 +5,11 @@ import speaker1 from "./speaker11.jpg";
 import speaker2 from "./speaker33.jpg";
 import speaker3 from "./speaker3.jpg";
 
-export { menu, close, speaker1, speaker2, speaker3 };
+export {
+  menu,
+  close,
+  Daksh_sethi_speaker1,
+  speaker2,
+  speaker3,
+  flagship_24_soon,
+};
